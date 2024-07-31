05358bc728720be2f3187e0b69b778d0438dcafa
neha161193 <neha.upadhyay727@gmail.com>
2024-08-01 00:41:40 +0530

    integration test

--- src/test/java/com/apibanking/AccountOpeningResourceTest.java
+++ src/test/java/com/apibanking/AccountOpeningResourceTest.java
@@ -70,13 +70,14 @@ class AccountOpeningResourceTest {
         }
     }
 
-    private void openAccount(String requestFileName, String panNo, String accountNo, String debitAccountNo, String customerId, String accountType) {
-        if (accountType == AccountType.FixedDeposit.name()){
+    private void openAccount(String requestFileName, String panNo, String accountNo, String debitAccountNo,
+            String customerId, String accountType) {
+        if (accountType == AccountType.FixedDeposit.name()) {
             Map<String, String> replacements = new HashMap<String, String>();
             replacements.put("${debitAccountNo}", debitAccountNo);
             replacements.put("${customerId}", customerId);
             sampleRequestBody = readAndReplacePlaceholders(requestFileName, replacements);
-        }else{
+        } else {
             Map<String, String> replacements = new HashMap<String, String>();
             replacements.put("${panNo}", panNo);
             sampleRequestBody = readAndReplacePlaceholders(requestFileName, replacements);
@@ -105,13 +106,22 @@ class AccountOpeningResourceTest {
                 .when().patch("/api/v1/accountservice/webhook-update")
                 .then()
                 .statusCode(202);
+
+        given()
+                .when().get("/api/v1/accountservice/" + applicationNo)
+                .then()
+                .statusCode(200)
+                .body("customerId", is(notNullValue()))
+                .body("status", is("Active"))
+                .body("type", is(accountType));
+
     }
 
     private String getCustomerId(){
-        return UUID.randomUUID().toString();
+        return UUID.randomUUID().toString().replace("-", "").toString();
     }
     private String getAccountNo(){
-        return UUID.randomUUID().toString().replace("-", "").substring(0, 16);
+        return String.valueOf(1000000 + new Random().nextInt(9000000)) + String.valueOf(100000000 + new Random().nextInt(900000000));
     }
     private String getPanNo(){
         return "BHJKM" + String.valueOf(1000 + new Random().nextInt(9000)) + "P";
@@ -160,8 +170,11 @@ class AccountOpeningResourceTest {
 
     @Test
     public void testGetAccountWithGoodCustomerId() {
+        String customerId = getCustomerId();
+        openAccount("saving-request.json", getPanNo(), getAccountNo(), null,customerId, AccountType.Saving.name());
+
         given()
-                .queryParam("customerId", "123123")
+                .queryParam("customerId", customerId)
                 .when().get("/api/v1/accountservice")
                 .then()
                 .statusCode(200)
@@ -169,5 +182,68 @@ class AccountOpeningResourceTest {
                 .body("productCode", is(notNullValue()))
                 .body("status", is(notNullValue()))
                 .body("type", is(notNullValue()));
+    }
+
+    @Test
+    public void testGetAccountWithGoodCustomerIdAndGoodAccountNo() {
+        String customerId = getCustomerId();
+        String accountNo = getAccountNo();
+
+        openAccount("saving-request.json", getPanNo(), accountNo, null,customerId, AccountType.Saving.name());
+
+        given()
+                .queryParam("customerId", customerId)
+                .queryParam("accountNo", accountNo)
+                .when().get("/api/v1/accountservice")
+                .then()
+                .statusCode(200)
+                .body("customerId", is(notNullValue()))
+                .body("productCode", is(notNullValue()))
+                .body("status", is(notNullValue()))
+                .body("type", is(notNullValue()));
+    }
+    @Test
+    public void testUpdateAccountStatus() {
+        String customerId = getCustomerId();
+        String accountNo = getAccountNo();
+
+        openAccount("saving-request.json", getPanNo(), accountNo, null,customerId, AccountType.Saving.name());
+        Map<String, String> replacements = new HashMap<String, String>();
+        replacements.put("${accountNo}", accountNo);
+        replacements.put("${accountType}", AccountType.Saving.name());
+
+        replacements.put("${customerId}", customerId);
+        replacements.put("${email}", "abc@gmail.com");
+        replacements.put("${mobileNo}", "9999999990");
+
+        sampleRequestBody = readAndReplacePlaceholders("update-account.json", replacements);
+
+        given()
+                .body(sampleRequestBody)
+                .contentType(ContentType.JSON)
+                .when().patch("/api/v1/accountservice")
+                .then()
+                .statusCode(200);
+
+                given()
+                .queryParam("customerId", customerId)
+                .queryParam("accountNo", accountNo)
+                .when().get("/api/v1/accountservice")
+                .then()
+                .statusCode(200)
+                .body(containsString("abc@gmail.com"))
+                .body(containsString("9999999990"));
+    }
+
+    @Test
+    public void testGetAccountWithGoodCustomerIdAndBadAccountNo() {
+        String customerId = getCustomerId();
+        openAccount("saving-request.json", getPanNo(), getAccountNo(), null,customerId, AccountType.Saving.name());
+        given()
+                .queryParam("customerId", customerId)
+                .queryParam("accountNo", getAccountNo())
+                .when().get("/api/v1/accountservice")
+                .then()
+                .statusCode(404);
     }
 }
